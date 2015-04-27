9c4eeb4d66b2084e1b6ca3f7f2d4a17ab90f3719
Vlad <vlad.costin_h>
2015-04-27 10:35:26 +0300

    the tables are not recreated anymore

--- CordovaMapsSample1/www/js/database/ddlStatement.js
+++ CordovaMapsSample1/www/js/database/ddlStatement.js
@@ -14,16 +14,20 @@ function createDatabase()
     ("Database Sample", "1.0", "Cordova Sample", 2 * 1024 * 1024);
     dbShell.transaction(createDB, errorCB, successDB);
     
+    
+    
   //    loadMainPage();
 }
 function createDB(tx)
 {
-    tx.executeSql('DROP TABLE IF EXISTS TRIP');
-    tx.executeSql('DROP TABLE IF EXISTS STORY');
+  //  tx.executeSql('DROP TABLE IF EXISTS TRIP');
+  //  tx.executeSql('DROP TABLE IF EXISTS STORY');
     
     tx.executeSql('CREATE TABLE IF NOT EXISTS TRIP(id integer primary key autoincrement,title TEXT,description TEXT)');
   //  tx.executeSql('CREATE TABLE IF NOT EXISTS STORY(id integer primary key autoincrement,title TEXT,description TEXT,date TEXT,Rate integer, idTrip NOT NULL,  FOREIGN KEY (idTrip) REFERENCES TRIP (id))');
     tx.executeSql('CREATE TABLE IF NOT EXISTS STORY(id integer primary key autoincrement,title TEXT,description TEXT,date TEXT,rate integer, idTrip) ');
+
+    selectQueryDBAllTrips();
 }
 
 function errorCB(tx)
@@ -33,11 +37,5 @@ function errorCB(tx)
 
 function successDB(tx)
 {
-    alert("YEAAAH");
-    
-
-  //  alert("am inserat ceva");
-    
-    //    tx.executeSql("select * from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = 'DEMO' ", [], renderList,errorCB);
-
+ //   alert("YEAAAH");
 }
