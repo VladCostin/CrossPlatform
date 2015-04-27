c5b206251eff97b03aee1c2e97f0b968eba421a7
Vlad <vlad.costin_h>
2015-04-27 16:15:15 +0300

    changed the structure of the table story, now it has a reference to trip  entrance ( foreign key)

--- CordovaMapsSample1/www/js/database/ddlStatement.js
+++ CordovaMapsSample1/www/js/database/ddlStatement.js
@@ -24,8 +24,8 @@ function createDB(tx)
   //  tx.executeSql('DROP TABLE IF EXISTS STORY');
     
     tx.executeSql('CREATE TABLE IF NOT EXISTS TRIP(id integer primary key autoincrement,title TEXT,description TEXT)');
-  //  tx.executeSql('CREATE TABLE IF NOT EXISTS STORY(id integer primary key autoincrement,title TEXT,description TEXT,date TEXT,Rate integer, idTrip NOT NULL,  FOREIGN KEY (idTrip) REFERENCES TRIP (id))');
-    tx.executeSql('CREATE TABLE IF NOT EXISTS STORY(id integer primary key autoincrement,title TEXT,description TEXT,date TEXT,rate integer, idTrip) ');
+    tx.executeSql('CREATE TABLE IF NOT EXISTS STORY(id integer primary key autoincrement,title TEXT,description TEXT,date TEXT,Rate integer, idTrip NOT NULL,  FOREIGN KEY (idTrip) REFERENCES TRIP (id))');
+  //  tx.executeSql('CREATE TABLE IF NOT EXISTS STORY(id integer primary key autoincrement,title TEXT,description TEXT,date TEXT,rate integer, idTrip) ');
 
     selectQueryDBAllTrips();
 }
